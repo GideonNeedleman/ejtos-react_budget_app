cf5f2d57b25ba56c16c9955ea23b118c5d353ee5
Gideon Needleman <111554925+GideonNeedleman@users.noreply.github.com>
2023-04-20 23:14:24 -0700

    trying to figure out how to edit budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,21 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget } = useContext(AppContext);
+    const handleChange = () => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: budget,
+        });
+    }
+    let newBudget = 2000;
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            {/* <span>Budget: £{budget}</span> */}
+            <label htmlFor="bud">£</label>
+            <input type="number" name="bud" id="bud" defaultValue="200" step={10} onChange={handleChange}/>
+            {/* this isn't working, reference how to enter allocation in AllocationForm.js line 68 */}
         </div>
     );
 };
